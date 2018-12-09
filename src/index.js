5d41b24ec96e108e9db9d8dc590a243cffba5ee8
Alex Burtson <alex@mag.cr>
2018-12-08 17:43:08 -0800

    fix local storage issue

--- src/index.js
+++ src/index.js
@@ -8,23 +8,37 @@ const clear = document.getElementById('clear');
 
 // if localStorage exists, itemsArray becomes the items in localStorage. otherwise, it is an empty array
 let itemsArray = localStorage.getItem('items') ? JSON.parse(localStorage.getItem('items')) : [];
+let spanLength = 0;
 
 // setItem adds key and value to local storage
 // JSON.stringify() converts the itemsArray data array to a string
 localStorage.setItem('items', JSON.stringify(itemsArray));
+localStorage.setItem('spanLength', JSON.stringify(spanLength))
 
 // JSON.parse converts the contents of localStorage back into something we can work with
 // getItem() retrieves a value by the key
 const data = JSON.parse(localStorage.getItem('items'));
 
-const createLI = text => {
+const createDeleteButton = li => {
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete');
+    li.appendChild(deleteButton);
+}
+
+const createTodo = text => {
     if (text !== '') {
         const li = document.createElement('li');
-        li.textContent = text;
+        const span = document.createElement('span');
+        span.textContent = text;
+        li.id = text;
+        li.appendChild(span);
         ul.appendChild(li);
+        console.log('Our created ul', ul)
+        createDeleteButton(li);
     }
 }
 
+// create todo when clicking submit button
 submit.addEventListener('click', event => {
     event.preventDefault();
 
@@ -34,16 +48,17 @@ submit.addEventListener('click', event => {
     // set localStorage to the new updated value
     localStorage.setItem('items', JSON.stringify(itemsArray));
 
-    createLI(input.value);
+    createTodo(input.value);
     input.value = '';
 });
 
 // loop through everything in our data variable, which contains all the existing localStorage data in a form JavaScript can understand
-// createLI() for each item again
+// createTodo() for each item again
 data.forEach(item => {
-    createLI(item);
+    createTodo(item);
 });
 
+// clear all todos
 clear.addEventListener('click', event => {
     event.preventDefault();
 
@@ -54,13 +69,66 @@ clear.addEventListener('click', event => {
     }
 });
 
+// remove todo
+ul.addEventListener('click', event => {
+    if (event.target.tagName === 'BUTTON') {
+        const deleteButton = event.target;
+        const li = deleteButton.parentNode;
+        const ul = li.parentNode;
 
+        ul.removeChild(li);
 
+        localStorage.removeItem('items', JSON.stringify(itemsArray));
+    }
+});
 
+// edit todo
+ul.addEventListener('click', event => {
+    const todoData = JSON.parse(localStorage.getItem('items'));
+    if (event.target.tagName === 'SPAN') {
+        const span = event.target;
+        const li = span.parentNode;
+        const text = span.textContent;
+        const input = document.createElement('input');
 
+        input.type = 'text';
+        input.value = '';
+        input.setAttribute('maxlength', 50);
+        li.insertBefore(input, span);
+        li.removeChild(span);
+        input.value = text;
+        createDeleteButton(input);
+    }
+});
 
-// Things todo with the todo list:
+const convertToSpan = event => {
+    const input = event.target;
+    const li = input.parentNode;
+    const text = input.value;
+    const span = document.createElement('span');
+    li.insertBefore(span, input);
+    li.removeChild(input);
+    span.textContent = text;
 
-// add edit button that shows delete buttons
+    itemsArray.push(text);
+    const oldData = JSON.parse(localStorage.getItem("items"));
+    const modifiedData = oldData.filter(todo => todo !== li.id);
+    modifiedData.push(text);
+    localStorage.setItem('items', JSON.stringify(modifiedData));
 
-// a way to edit the text after it's been added
+    createDeleteButton(input);
+}
+
+// convert input back to span when hitting ENTER
+ul.addEventListener('keyup', event => {
+    if (event.keyCode === 13) {
+        convertToSpan(event);
+    }
+}); 
+
+// convert input back to span when mouse leaves input
+ul.addEventListener('mouseout', event => {
+    if (event.target.tagName === 'INPUT') {
+        convertToSpan(event);
+    }
+}); 
